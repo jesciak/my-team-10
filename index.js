1fa3f695f5e926a6ed9645622fa36e7373de1c70
jesciak <jesciak@yahoo.com>
2022-11-15 23:09:09 -0500

    html page is rendering

--- index.js
+++ index.js
@@ -68,182 +68,53 @@ const questions = [
         when: function (response) {
             return response.role === "Intern";
         }
-    }];
+    },
+    {
+    type: "confirm",
+        name: "done",
+        message: "Add another employee?",
+        default: true,
+    },
+];
+async function inputEmployees(employeeData= []){
+    try{
+        const{ done, ...response}= await inquirer.prompt(questions);
+        const newEmployee= [...employeeData, response];
+        return done ? inputEmployees(newEmployee) : newEmployee;
+    }catch(err){
+        throw err;
+    }
+    }
+    async function buildTeam(){
+        try{
+            const teamMembers= [];
+            const dataInput= await inputEmployees();
 
+            dataInput.map((employee)=>{
+                const{ name, id, email, role, officeNumber, github, school} =employee;
 
-async function userResponse() {
-    await inquirer.prompt(questions)
-    .then(function (answer) {
-
-        if (answer.role === "Manager") {
-            const newManager = new Manager(answer.name, answer.id, answer.email, answer.officeNumber);
+        if (role === "Manager") {
+            const newManager = new Manager(name, id, email, officeNumber);
             teamMembers.push(newManager);
-            addTeamMembers();
+         
         }
 
-        else if (answer.role === "Engineer") {
-            const newEngineer = new Engineer(answer.name, answer.id, answer.email, answer.github);
+        else if (role === "Engineer") {
+            const newEngineer = new Engineer(name, id, email, github);
             teamMembers.push(newEngineer);
-            addTeamMembers();
+           
         }
         else {
-            const newIntern = new Intern(answer.name, answer.id, answer.email, answer.school);
-            teamMembers.push(newIntern);
-            addTeamMembers();
+            const newIntern = new Intern(name, id, email, school);
+            teamMembers.push(newIntern); 
         }
-        
     });
+
+   const renderTeam= render(teamMembers);
+   fs.writeFile(outputPath, renderTeam, ()=> console.log('SUCCESS!'));
+} catch (err){
+    throw err;
 }
+    }
+buildTeam();
 
-
-
-function addTeamMembers() {
-     inquirer.prompt([
-        {
-            type: 'confirm',
-            name: 'addTeamMembers',
-            message: "Do you want to add another employee?",
-            default: true
-        }
-    ])
-    .then(function (response) {
-        // console.log(response)
-        if (response.addTeamMembers) {
-            // console.log('TEAM MEMBERS ON YES', teamMembers)
-            userResponse();
-        }
-        else {
-            // const html = render(teamMembers);
-            // console.log('TEAM MEMBERS', teamMembers)
-            buildTeam()
-
-        }
-    })
-}
-    
-function buildTeam(){
-    fs.writeFile(outputPath, 'utf8', render(teamMembers),(err) => {
-
-        // if (err) throw err;
-        console.log("Success!");
-    })
-}
-
-userResponse();
-    //     else {
-
-    //         const htmlpage = (data) =>{
-    //         return new Promise ((resolve, reject)=>{
-    //         fs.writeFile("./dist/index.html", data, (err) => {
-    //             if (err) {
-    //                 reject(err);
-    //                 return;
-    //             }
-    //             resolve({
-    //                 ok: true,
-    //                 message: "HTML page has been generated.",
-    //             });
-    //         });
-        
-    //         });
-    //     }
-    // }
-    // function init(){
-    //     questions()
-    //     .then((response)=>{
-    //         return render.htmlpage(response);
-    //     })
-    //     .then((res)=>{
-    //         htmlpage(res);
-    //         console.log("Success! Check out your page!")
-
-    //     });
-    // }
-    // init();
-
-    
-
-
-
-
-// const managerQuestion = [
-//     {
-//         type: 'input',
-//         name: 'officeNumber',
-//         message: "Enter the manager office number"
-
-//     }
-// ];
-
-// function addManager() {
-//     inquirer.prompt([...questions, ...managerQuestion])
-//         .then(function (answer) {
-//             const newManager = new Manager(answer.name, answer.id, answer.email, answer.officeNumber)
-//             teamMembers.push(newManager)
-//             addTeamMembers()
-
-//         })
-// }
-// addManager()
-
-// function addTeamMembers() {
-//     inquirer.prompt([
-//         {
-//             type: 'list',
-//             name: 'addType',
-//             message: "Select which type of team member you would like to add.",
-//             choices: ["Add Engineer", "Add Intern", "Exit App"]
-//         }
-//     ]).then(function (response) {
-//         switch (response.addType()) {
-//             case "Add Engineer":
-//                 addEngineer()
-//                 break;
-//             case "Add Intern":
-//                 addIntern()
-//                 break;
-//             case "Exit App":
-//                 addExitApp()
-//                 break;
-//         }
-//     })
-// }
-// const internQuestion = [
-//     {
-//         type: 'input',
-//         name: 'school',
-//         message: "Enter the name of the intern's school"
-
-//     }
-// ];
-// function addIntern() {
-//     inquirer.prompt([...questions, ...internQuestion])
-//         .then(function (answer) {
-//             const newIntern = new Intern(answer.name, answer.id, answer.email, answer.school)
-//             teamMembers.push(newIntern)
-//             addTeamMembers()
-//         })
-// }
-
-
-// addIntern()
-
-// const engineerQuestion = [
-//     {
-//         type: 'input',
-//         name: 'github',
-//         message: "Enter the name of the engineer's github profile"
-
-//     }
-// ];
-// function addEngineer() {
-//     inquirer.prompt([...questions, ...engineerQuestion])
-//         .then(function (answer) {
-//             const newEngineer = new Engineer(answer.name, answer.id, answer.email, answer.github)
-//             teamMembers.push(newEngineer)
-//             addTeamMembers()
-//         })
-// }
-
-
-// addEngineer()
--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,33 +1,59 @@
 const path = require("path");
 
 const fs = require('fs')
+const mainDataDir = path.resolve(__dirname, "../dist");
 
-function insertValue(template, valueName, value){
-    let reg= new RegExp(`{{ ${valueName}}}`, 'g')
-    return template.replace(reg, value)
+const render = teamMembers => {
+    const html = [];
+    html.push(teamMembers.filter(employee => employee.getRole() === 'Manager').map(manager => renderManager(manager)));
+    html.push(teamMembers.filter(employee => employee.getRole() === 'Engineer').map(engineer => renderEngineer(engineer)));
+    html.push(teamMembers.filter(employee => employee.getRole() === 'Intern').map(intern => renderIntern(intern)));
 
+    return renderMain(html.join(''));
+};
+
+const renderManager = manager => {
+    let mainData = fs.readFileSync(path.resolve(mainDataDir, 'manager.html'), 'utf8');
+    mainData = insertValue(mainData, 'name', manager.getName());
+    mainData = insertValue(mainData, 'role', manager.getRole());
+    mainData = insertValue(mainData, 'id', manager.getId());
+    mainData = insertValue(mainData, 'email', manager.getEmail());
+    mainData = insertValue(mainData, 'officeNumber', manager.getOfficeNumber());
+    return mainData;
+};
+const renderEngineer = engineer => {
+    let mainData = fs.readFileSync(path.resolve(mainDataDir, 'engineer.html'), 'utf8');
+    mainData = insertValue(mainData, 'name', engineer.getName());
+    mainData = insertValue(mainData, 'role', engineer.getRole());
+    mainData = insertValue(mainData, 'id', engineer.getId());
+    mainData = insertValue(mainData, 'email', engineer.getEmail());
+    mainData = insertValue(mainData, 'github', engineer.getGithub());
+    return mainData;
+};
+const renderIntern = intern => {
+    let mainData = fs.readFileSync(path.resolve(mainDataDir, 'intern.html'), 'utf8');
+    mainData = insertValue(mainData, 'name', intern.getName());
+    mainData = insertValue(mainData, 'role', intern.getRole());
+    mainData = insertValue(mainData, 'id', intern.getId());
+    mainData = insertValue(mainData, 'email', intern.getEmail());
+    mainData = insertValue(mainData, 'school', intern.getSchool());
+    return mainData;
 }
 
+// function insertValue(template, valueName, value) {
+//     let reg = new RegExp(`{{ ${valueName}}}`, 'g')
+//     return template.insertValue(reg, value)
 
-function render(teamMembers){
-    // console.log('team members', teamMembers)
-    let outputHTML = ''
-    for (const member of teamMembers) {
-        console.log('MEMBERS ',member)
-        // const OUTPUT_DIR= path.resolve(__dirname, "dist");
-        const outputPath = path.join(__dirname, "../dist");
+// }
 
-        let data = fs.readFileSync(outputPath, '')
-        data =insertValue(data, 'name', member.getName())
-        outputHTML+= data
-    }
-
-    let templatePath = path.resolve(__dirname, '../dist/index.html')
-    let mainData = fs.readFileSync(templatePath, 'utf-8')
-
-    return insertValue(mainData, 'team', outputHTML)
+const renderMain = html => {
+    const mainData = fs.readFileSync(path.resolve(mainDataDir, 'main.html'), 'utf8');
+    return insertValue(mainData, 'team', html)
+};
+const insertValue = (mainData, valueName, value) => {
+    const reg = new RegExp(`{{ ${valueName} }}`, 'g');
+    return mainData.replace(reg, value)
 }
-
 
 
 module.exports = render;
